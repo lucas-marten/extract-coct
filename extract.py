5e5f5798b22b0430efc5126693c8607260c2f09f
Lucas Marten <lucas.marten@climatempo.com.br>
2025-02-28 15:15:34 -0300

    add: all stacks

--- extract.py
+++ extract.py
@@ -12,7 +12,6 @@ import send_mail
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Extract climate data.")
     parser.add_argument("--date", type=str, help="Date in ISO format (YYYY-MM-DD)")
-    parser.add_argument("--points", type=str, help="Path to CSV file with points")
     parser.add_argument(
         "--output_dir", type=str, help="Directory to save the output CSV files"
     )
@@ -20,41 +19,44 @@ def parse_arguments():
 
 
 def main(date, path_points, output_dir):
-    df_points = pd.read_csv(path_points)
+    stacks = ['capitais.csv', 'eptvmg.csv', 'eptvsc.csv', 'rpc.csv',
+                    'tvmorena.csv', 'eptvkp.csv', 'eptvrp.csv', 'rbs.csv', 'tvca.csv']
 
     init = date.strftime('%Y%m%dT00')
-    end = (date + timedelta(days=5)).strftime('%Y%m%dT23')
+    end = (date + timedelta(days=3)).strftime('%Y%m%dT23')
 
     os.makedirs(output_dir, exist_ok=True)
 
     outputs = list()
     variables = ["total_precipitation", "2m_air_temperature"]
-    for variable in variables:
-        path_in = date.strftime(
-            f"/data/forecast/chimera_as/{variable}/%Y/%j/chimera_as_{variable}_M000_%Y%m%d00.nc"
-        )
+    for stack in stacks:
+        df_points = pd.read_csv(f'/airflow/tools/extract-coct/static/{stack}')
+        for variable in variables:
+            path_in = date.strftime(
+                f"/data/forecast/chimera_as/{variable}/%Y/%j/chimera_as_{variable}_M000_%Y%m%d00.nc"
+            )
 
-        lats = df_points.lat.to_list()
-        lons = df_points.lon.to_list()
-        ids = df_points.name.to_list()
+            lats = df_points.lat.to_list()
+            lons = df_points.lon.to_list()
+            ids = df_points.name.to_list()
 
-        ds = xr.open_dataarray(path_in).sel(time=slice(init, end))
-        ds_times = pd.to_datetime(ds.time.values) - timedelta(hours=3)
-        print(ds_times)
-        dfs = list()
-        for i in range(len(ids)):
-            prec_values = ds.sel(
-                longitude=lons[i], latitude=lats[i], method="nearest"
-            ).values
-            df = pd.DataFrame({ids[i]: prec_values}, index=ds_times)
-            dfs.append(df)
+            ds = xr.open_dataarray(path_in).sel(time=slice(init, end))
+            ds_times = pd.to_datetime(ds.time.values)
 
-        points_name = os.path.basename(path_points).split('.')[0]
-        path_out = os.path.join(date.strftime(output_dir), f"{points_name}_{variable}.csv")
-        os.makedirs(os.path.dirname(path_out), exist_ok=True)
-        pd.concat(dfs, axis=1).to_csv(path_out)
-        print(f"created: {path_out}")
-        outputs.append(path_out)
+            dfs = list()
+            for i in range(len(ids)):
+                prec_values = ds.sel(
+                    longitude=lons[i], latitude=lats[i], method="nearest"
+                ).values
+                df = pd.DataFrame({ids[i]: prec_values}, index=ds_times)
+                dfs.append(df)
+
+            points_name = os.path.basename(path_points).split('.')[0]
+            path_out = os.path.join(date.strftime(output_dir), f"{points_name}_{variable}.csv")
+            os.makedirs(os.path.dirname(path_out), exist_ok=True)
+            pd.concat(dfs, axis=1).to_csv(path_out)
+            outputs.append(path_out)
+            print(f"created: {path_out}")
 
     return outputs
 
@@ -62,6 +64,5 @@ def main(date, path_points, output_dir):
 if __name__ == "__main__":
     args = parse_arguments()
     date = datetime.fromisoformat(str(args.date))
-
     outputs = main(date, args.points, args.output_dir)
     send_mail.run(outputs, subject='Extração - COCT')
